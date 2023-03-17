6c1716167074dcdef42a3b73375e524bf32d7c2d
haowenmvp <panhaowen789@gmail>
2023-03-17 14:48:41 -0500

    add some text

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1> Welcome to Amplify hhhhhdfdfkfldskjvfdjksdfjsefk</h1>
+      <h1> Welcome to Amplify hhhhhdfdfkfldskjvfdjksdfjsefdgfdfgk</h1>
     </div>
   );
 }
