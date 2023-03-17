3f6b1e766a291954e7d8acafabdb675c91ebedca
haowenmvp <panhaowen789@gmail>
2023-03-17 12:33:18 -0500

    delete some text

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1> Welcome to Amplify hhhhhdfdfkflds</h1>
+      <h1> Welcome to Amplify hhhhhdfdfkfldskjvfdjksdfjsefk</h1>
     </div>
   );
 }
